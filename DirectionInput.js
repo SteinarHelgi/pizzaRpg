02546125bf4ada35dc95ea2eb80d0aba24f2bb95
Steinar Helgi <steinarhelgi@outlook.com>
2025-08-12 13:35:19 +0000

    fix direction input listeners

--- DirectionInput.js
+++ DirectionInput.js
@@ -15,26 +15,29 @@ class DirectionInput {
 
 	}
 
-	get direction() {
-		return this.heldDirections[0]
-	}
-	init() {
-		document.addEventListener("keydown", e => {
-			const dir = this.map[e.key]
-			if (dir && this.heldDirections.indexOf(dir) == -1) {
-				this.heldDirections.unshift(dir)
-			}
-		})
-		document.addEventListener("keyup", e => {
-			const dir = this.map[e.key]
-			const index = this.heldDirections.indexOf(dir)
-			if (index > -1) {
-				this.heldDirections.splice(index, 1)
-			}
-		})
-	}
-	destroy() {
-		document.removeEventListener("keydown", this.onKeyDown);
-		document.removeEventListener("keyup", this.onKeyUp);
-	}
+        get direction() {
+                return this.heldDirections[0]
+        }
+        init() {
+                this.onKeyDown = (e) => {
+                        const dir = this.map[e.key]
+                        if (dir && this.heldDirections.indexOf(dir) == -1) {
+                                this.heldDirections.unshift(dir)
+                        }
+                }
+                this.onKeyUp = (e) => {
+                        const dir = this.map[e.key]
+                        const index = this.heldDirections.indexOf(dir)
+                        if (index > -1) {
+                                this.heldDirections.splice(index, 1)
+                        }
+                }
+
+                document.addEventListener("keydown", this.onKeyDown)
+                document.addEventListener("keyup", this.onKeyUp)
+        }
+        destroy() {
+                document.removeEventListener("keydown", this.onKeyDown);
+                document.removeEventListener("keyup", this.onKeyUp);
+        }
 }
